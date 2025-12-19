00dd08dca7e50153808aee65eedf013c17a55265
deyplay77 <1of1lagos@gmail.com>
2025-12-19 10:46:32 +0100

    Update SimpleResponder.sol

--- src/SimpleResponder.sol
+++ src/SimpleResponder.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.20;
 
 contract SimpleResponder {
-    function respondCallback(uint256 amount) public {
+    // Changed from public to external (better convention per review)
+    function respondCallback(uint256 amount) external {
         // PoC: The Trap triggered, the Responder was called.
     }
 }
